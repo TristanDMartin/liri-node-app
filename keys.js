e1ef681c9a90080ea60fec12376f5c5f7594bf09
Tristan Martin <smove50@gmail.com>
2019-02-09 18:01:48 -0500

    updating

---
+++ keys.js
@@ -0,0 +1,13 @@
+// console.log('this is loaded');
+
+exports.spotify = {
+	id: '5cd1d172f98d42cd9296df65e920a800',
+	secret: '13d2cf2f85d04119bdebc354afceef57',
+}
+
+// console.log('this is loaded');
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
